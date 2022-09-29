88c9e32c3b42e565e1095ef3655ba3dde5a2cc63
danielm19 <danielm19@csu.fullerton.edu>
2022-09-29 15:07:39 -0700

    added start screen, button highlight function

--- button.py
+++ button.py
@@ -1,46 +1,47 @@
 import pygame as pg
-
-#set colors as class variable 
-green = (0, 200, 0)
-white = (255, 255, 255)
-blue = (0, 0, 200)
-light_blue = (100, 150, 200)
+from settings import Settings
+import sys
 
 class Button:
-    def __init__(self, screen, message):
-        #initialize buttion attributes 
+    def __init__(self, screen, text, x, y):
         self.screen = screen
         self.screen_rect = screen.get_rect()
+        self.settings = Settings()
+        self.x = x
+        self.y = y
         
-        #setup the button properties and size 
+        #set the button dimensions
         self.width, self.height = 200, 50
-        self.button_color = blue
-        self.text_color = white
-        self.font = pg.font.SysFont(None, 48)
+        self.button_color = self.settings.black
+        self.text_color = self.settings.green
+        self.font = pg.font.SysFont('cambria', 40)
         
-        #create the button rect and center it and prep the button message
+        #make the button rect and center it 
         self.rect = pg.Rect(0, 0, self.width, self.height)
-        self.rect_center = self.screen_rect.center
+        self.rect.center = (self.x, self.y)
         
-        self.msg_image = self.font.render(message, True, self.text_color, self.button_color)
-        self.msg_image_rect = self.msg_image.get_rect()
-        self.msg_image_center = self.rect.center
+        self.hover_color = self.settings.blue
+        self.hover_image = self.font.render(text, True, self.text_color, self.hover_color)
         
-        self.hover_color = light_blue
-        self.hover_image = self.font.render(message, True, self.text_color, self.hover_color)
+        self.prep_text(text)
+    
+    def prep_text(self, text):
+        self.text_image = self.font.render(text, True, self.text_color, self.button_color)
+        self.text_image_rect = self.text_image.get_rect()
+        self.text_image_rect.center = self.rect.center
         
-            
     def draw(self):
-        #draw a blank button and draw the message to display 
         self.screen.fill(self.button_color, self.rect)
-        self.screen.blit(self.msg_image, self.msg_image_rect)
+        self.screen.blit(self.text_image, self.text_image_rect)
         
-        mouse_point = pg.mouse.get_pos()
-        if self.rect.collidepoint(mouse_point):
-            if self.button_color == blue:
+        mouse_pos = pg.mouse.get_pos()
+        if self.rect.collidepoint(mouse_pos):
+            if self.button_color == self.settings.black:
                 self.screen.fill(self.hover_color, self.rect)
-                self.screen.blit(self.hover_image, self.msg_image_rect)
-            
+                self.screen.blit(self.hover_image, self.text_image_rect)
+        
+
+
         
             
         
--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 import pygame as pg
 from settings import Settings
 import game_functions as gf
-
 from laser import Lasers, LaserType
 from alien import Aliens
 from ship import Ship
@@ -30,7 +29,8 @@ class Game:
         self.ship = Ship(game=self)
         self.aliens = Aliens(game=self)
         self.settings.initialize_speed_settings()
-        self.play_button = Button(screen= self.screen, message= "Play")
+        self.play_button = Button(screen= self.screen, text= "Play", x = self.settings.screen_width // 2, y = 625)
+        self.highscore_button = Button(screen= self.screen, text= "Highscores", x = self.settings.screen_width // 2, y = 675)
 
     def reset(self):
         print('Resetting game...')
@@ -44,6 +44,39 @@ class Game:
         self.sound.gameover()
         pg.quit()
         sys.exit()
+        
+    def start_screen(self):
+        font = pg.font.SysFont('arial', 72)
+        text = font.render('SPACE', True, self.settings.white)
+        text_rect = text.get_rect()
+        text_rect.center = ((self.settings.screen_width // 2, self.settings.screen_height // 10))
+        
+        font1 = pg.font.SysFont('arial', 32)
+        invader_txt = font.render('INVADERS', True, self.settings.green)
+        invader_txt_rect = invader_txt.get_rect()
+        invader_txt_rect.center = ((self.settings.screen_width // 2, self.settings.screen_height // 5))
+        
+        button_click = False
+        
+        
+        while button_click == False:
+            self.screen.fill(self.settings.black)
+            self.screen.blit(text, text_rect)
+            self.screen.blit(invader_txt, invader_txt_rect)
+            self.play_button.draw()
+            self.highscore_button.draw()
+            
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    pg.quit()
+                elif event.type == pg.MOUSEBUTTONDOWN:
+                    mousepos = pg.mouse.get_pos()
+                    if self.play_button.rect.collidepoint(mousepos):
+                        button_click = True
+                        self.play() 
+            pg.display.update()
+                    
+
 
     def play(self):
         self.sound.play_bg()
@@ -59,7 +92,8 @@ class Game:
 
 def main():
     g = Game()
-    g.play()
+    g.start_screen()
+    #g.play()
 
 
 if __name__ == '__main__':
